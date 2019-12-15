05ada1be933beb34baeb8575f5636de97af8af27
Thomas Buch Andersson <t.b.andersson1@gmail.com>
2019-12-16 09:14:04 +1100

    4 combinations working

--- Truck_combinations2.py
+++ Truck_combinations2.py
@@ -1,47 +1,64 @@
-limit = 200
+limit = 100
 count = 0
 
 single = [0,1,2,3,4,5,6,7,8,9,10]
+singletwo = [0,1,2,3,4,5,6,7,8,9,10]
 bdouble = [0,1,2,3,4,5,6,7,8,9,10]
 extra = [0,1,2,3,4,5,6,7,8,9,10]
 
 total_combinations = []
 
 spallet = 0
+stwopallet = 0
 bpallet = 0
 epallet = 0
 tpallet = 0
 
 for s in single:
-    spallet = s*24
+    spallet = s*22
     tpallet += spallet
     firstround = tpallet
     if tpallet < limit:
-        for b in bdouble:
-            if b != 0:
-                bpallet = 36
-            tpallet += bpallet
+        for stwo in singletwo:
+            if stwo != 0:
+                stwopallet = 24
+            tpallet += stwopallet
             secondround = tpallet
             if tpallet < limit:
-                for e in extra:
-                    if e != 0:
-                        epallet = 1
-                    tpallet += epallet
+                for b in bdouble:
+                    if b != 0:
+                        bpallet = 36
+                    tpallet += bpallet
+                    thirdround = tpallet
                     if tpallet < limit:
-                        total_combinations.append([s,b,e,tpallet])
-                        print([s,b,e])
-                        print(tpallet)
+                        for e in extra:
+                            if e != 0:
+                                epallet = 1
+                            tpallet += epallet
+                            if tpallet < limit:
+                                total_combinations.append([s,stwo,b,e,tpallet])
+                                print([s,stwo,b,e])
+                                print(tpallet)
+                            else:
+                                print("BREAK Extra")
+                                tpallet = thirdround
+                                stwopallet = 0
+                                bpallet = 0
+                                epallet = 0
+                                break
                     else:
-                        print("SEEE HERE!!")
+                        print("BREAK b")
                         tpallet = secondround
+                        stwopallet = 0
                         bpallet = 0
                         epallet = 0
                         break
                 tpallet = secondround
                 epallet = 0
             else:
-                print("SEE HERE 2 !!!")
+                print("BREAK s2")
                 tpallet = firstround
+                stwopallet = 0
                 bpallet = 0
                 epallet = 0
                 break
