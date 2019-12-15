c9d05caf3b40b1bcbf6c469cee8efda9967aa5d1
Thomas Buch Andersson <t.b.andersson1@gmail.com>
2019-12-16 08:38:30 +1100

    Update for 3 combinations

--- Truck_combinations2.py
+++ Truck_combinations2.py
@@ -1,33 +1,54 @@
-limit = 150
+limit = 200
 count = 0
 
-single = [0,1,2,3,4,5]
-bdouble = [0,1,2,3,4,5]
-extra = [0,1,2,3,4,5]
+single = [0,1,2,3,4,5,6,7,8,9,10]
+bdouble = [0,1,2,3,4,5,6,7,8,9,10]
+extra = [0,1,2,3,4,5,6,7,8,9,10]
 
 total_combinations = []
 
 spallet = 0
 bpallet = 0
+epallet = 0
 tpallet = 0
 
 for s in single:
     spallet = s*24
     tpallet += spallet
+    firstround = tpallet
     if tpallet < limit:
         for b in bdouble:
             if b != 0:
                 bpallet = 36
             tpallet += bpallet
+            secondround = tpallet
             if tpallet < limit:
-                total_combinations.append([s,b,tpallet])
-                print([s,b])
-                print(tpallet)
+                for e in extra:
+                    if e != 0:
+                        epallet = 1
+                    tpallet += epallet
+                    if tpallet < limit:
+                        total_combinations.append([s,b,e,tpallet])
+                        print([s,b,e])
+                        print(tpallet)
+                    else:
+                        print("SEEE HERE!!")
+                        tpallet = secondround
+                        bpallet = 0
+                        epallet = 0
+                        break
+                tpallet = secondround
+                epallet = 0
             else:
-                tpallet = 0
+                print("SEE HERE 2 !!!")
+                tpallet = firstround
                 bpallet = 0
+                epallet = 0
                 break
+
 print(total_combinations)
+
+print(len(total_combinations))
 
 
 #for s in single:
